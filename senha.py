dfbc0138f037e7d066f629bb64e380cb1912ba2a
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-04 12:13:49 -0300

    desenvolvimento de sistemas com rpa, para evoluir mais na carreira de desenvolvimento e programação de sistema

---
+++ senha.py
@@ -0,0 +1,12 @@
+import pypdf
+
+pdf = pypdf.PdfWriter(clone_from="o_cortico.pdf")
+
+# proteger o PDF com senha
+pdf.encrypt("112233")
+
+pdf.write("o_cortico_senha.pdf")
+
+# lendo arquivos PDF com senha
+leitor = pypdf.PdfReader("o_cortico_senha.pdf")
+
--- transformar.py
+++ transformar.py
@@ -4,6 +4,19 @@ pdf = pypdf.PdfWriter(clone_from="o_cortico.pdf")
 
 transformacao = pypdf.Transformation()
 
-pdf.pages[0].add_transformation(transformacao.rotate(50))
+# rotacionar o conteudo
+pdf.pages[0].add_transformation(transformacao
+                                  .rotate(50)
+                                  .translate(tx=400))
+
+
+# movimentar o conteudo
+pdf.pages[1].add_transformation(transformacao.translate(tx=400, ty=500))
+
+pdf.pages[2].add_transformation(transformacao.scale(sx=0.5, sy=0.8))
+
+#alterar a escala da paigna
+pdf.pages[3].scale_by(1.5)
 
 pdf.write("PDF Transformacao.pdf")
+
