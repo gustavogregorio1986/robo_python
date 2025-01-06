306a50357e677acaf61818a55589d938a301503c
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-05 21:57:54 -0300

    continuando a extração de texto para aorender ainda mais e crescer profissionalmente sempre evoluindo da melhor forma numa evolução profunda

--- extraindo_texto.py
+++ extraindo_texto.py
@@ -3,3 +3,12 @@ import pypdf
 leitor = pypdf.PdfWriter("o_cortico.pdf")
 
 print(leitor.pages[0].extract_text())
+
+textos = []
+for pagina in leitor.pages:
+    textos.append(pagina.extract_text())
+
+
+texto = ''.join(textos)
+
+print(texto)
